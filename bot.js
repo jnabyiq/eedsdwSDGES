cc39c52003d64ac57d7b9066394f41825696ac5a
JnabY <43906195+jnabyiq@users.noreply.github.com>
2018-10-10 02:14:06 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -342,7 +342,7 @@ client.on('message', message => {
     }
 });
 
-const adminprefix = "!";
+const adminprefix = "";
 const devs = ['323160008411971585','323160008411971585'];
 client.on('message', message => {
   var argresult = message.content.split(` `).slice(1).join(' ');
@@ -366,5 +366,30 @@ if (message.content.startsWith(adminprefix + 'setT')) {
     message.channel.sendMessage(`**تم تغيير تويتش البوت إلى  ${argresult}**`)
 }
 });
+
+
+const Discord = require("discord.js")
+const client = new Discord.Client();
+const YTDL = require('ytdl-core');
+const nodeopus = require('node-opus');
+const ffmpeg = require('ffmpeg');
+var servers = {};
+function play(connection, message, args) {
+  var server = servers[message.guild.id];
+  server.dispatcher = connection.playStream(YTDL(args[0]), {filter: "audioonly"});
+  server.queue.shift();
+  server.dispatcher.on("end", function() {
+    if (server.queue[0]) play(connection, message);
+    else connection.disconnect();
+  });
+}
+
+
+client.on('message', message =>{
+  if(message.content.startsWith('join')){
+    const voiceChannel = message.member.voiceChannel
+    voiceChannel.join();
+    message.channel.send("تم الأتصال بالروم الصوتي")
+}})
 
 client.login(process.env.BOT_TOKEN);
